d9b7cbf843f92f9a31adeaa70868dbc3dfa2ff85
DixitAnjali01 <122504032+DixitAnjali01@users.noreply.github.com>
2023-01-12 09:08:23 +0530

    Add files via upload

---
+++ program25.c
@@ -0,0 +1,10 @@
+//program to enter any number and calculate its square root
+#include<stdio.h>
+#include<math.h>
+int main(){
+    float num;
+    printf("enter any number = ");
+    scanf("%f",&num);
+    printf("the square root of the number is = %f ",sqrt(num));
+    return 0;
+}
