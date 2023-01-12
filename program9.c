a6f882683d07d19408479712ddd45d3ec04dd2f9
DixitAnjali01 <122504032+DixitAnjali01@users.noreply.github.com>
2023-01-12 09:00:40 +0530

    Add files via upload

---
+++ program9.c
@@ -0,0 +1,12 @@
+//program to swap two numbers
+#include<stdio.h>
+int main()
+    { int a=1,b=2,c;
+    printf("The numbers before swapping are %d, %d \n ",a,b);
+    c=a;
+    a=b;
+    b=c;
+    printf("the numbers after swapping are %d ,%d ",a,b);
+    return 0;
+
+    }
