66f7af6d785a0d8f55ad51795fc0ba309d32fb9b
DixitAnjali01 <122504032+DixitAnjali01@users.noreply.github.com>
2023-01-12 09:11:04 +0530

    Add files via upload

---
+++ program35.c
@@ -0,0 +1,10 @@
+//program to print a particular line
+#include<stdio.h>
+int main(){
+    float cost;
+    cost=172.53;
+    printf("The sales total is : $ ");
+   
+    printf("%0.2f",cost);
+    return 0;
+}
