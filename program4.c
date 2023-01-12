623be4b5a284ba5cdccc185fcaccbdd7b33f6f14
DixitAnjali01 <122504032+DixitAnjali01@users.noreply.github.com>
2023-01-12 08:59:03 +0530

    Add files via upload

---
+++ program4.c
@@ -0,0 +1,12 @@
+//program to multiply two floating point numbers
+#include<stdio.h>
+int main()
+{
+    float n1 , n2;
+    printf("enter the first no. = ");
+    scanf("%f",&n1);
+    printf("enter the second no. = ");
+    scanf("%f",&n2);
+    printf("The product of the two no. is = %f",n1*n2);
+    return 0;
+}
