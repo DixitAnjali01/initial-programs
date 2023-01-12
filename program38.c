933dcb5c406e20ed9d8e312f962e848049e59bb8
DixitAnjali01 <122504032+DixitAnjali01@users.noreply.github.com>
2023-01-12 09:11:31 +0530

    Add files via upload

---
+++ program38.c
@@ -0,0 +1,8 @@
+//population of a city is 30,000 and it increase by 20% by first year and 30% by second year we have to find the population at the end of two years
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int P=30000;
+    printf(" THE POPULATION AT THE END OF TWO YEARS IS = %d",(P+6000+9000)); // 20% of 30000=6000 and 30% of 30000=9000
+    return 0;
+}
